82abcf7e4430ae6b0a5159032ccb82a7019fd3d6
leilus <prettytigerr@gmail.com>
2023-04-24 20:14:37 +0200

    Website trial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import BubbleSort from './pages/BubbleSort';
+import Home from './Home';
+import 'animate.css';
 
 function App() {
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <div className='App'>
+  
+   <div className="header">
+            <div className="nav">
+                <h1>CHOOSE YOUR ALGORITHM</h1>
+                <ul>
+                    <li><span><Link style={{ textDecoration: 'none', color: 'black' }} to="/bubblesort">BUBBLE SORT</Link></span></li>
+                    <li><span><Link style={{ textDecoration: 'none', color: 'black' }} to="/selectionsort">SELECTION SORT</Link></span></li>
+                    <li><span><Link style={{ textDecoration: 'none', color: 'black' }} to="/insertionsort">INSERTION SORT</Link></span></li>
+                </ul>
+            </div>
+        </div>
+    
+ 
+  <Routes>
+    <Route exact path='/home' element={<Home />} />
+    <Route path='/bubblesort' element={<BubbleSort />} />
+    <Route path='/selectionsort' element={<BubbleSort />} />
+    <Route path='/insertionsort' element={<BubbleSort />} />
+  </Routes>
+
+ 
+   </div>
+   </BrowserRouter>
   );
 }
 
